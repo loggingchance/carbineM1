2edf352dc894224c548c1d297ea3d6110eaeaf4f
loggingchance <77699599+loggingchance@users.noreply.github.com>
2026-08-08 18:26:59 -0600

    feat: show microfvs cloud endpoint in run panel (#8)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_CARBINE_FVS_API_URL?: string;
   readonly VITE_CARBINE_LOCAL_FVS_URL?: string;
+  readonly VITE_CARBINE_MICROFVS_API_URL?: string;
   readonly VITE_CARBINE_BUILD_ID?: string;
 }
 
